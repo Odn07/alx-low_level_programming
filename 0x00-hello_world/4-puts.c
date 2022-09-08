5daf05a7e90eaae51e86c7394594f9fdf9187957
Emmanuel Odion <odione07@gmail.com>
2022-09-08 14:12:39 -0700

    Print a string in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+/*A program to use put() to print to stdout*/
 int main()
 {	
-	char putsVal[] = "\"Programming is like building a multilingual puzzle";
+	char putsVal[]="\"Programming is like building a multilingual puzzle";
+
 	puts(putsVal);
-	return 0;
+	return (0);
 }
