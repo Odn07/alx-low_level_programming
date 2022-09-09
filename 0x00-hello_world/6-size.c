1d8c3b029db5a451a1263e36f0c2e92e82c20d4f
Emmanuel Odion <odione07@gmail.com>
2022-09-08 18:03:37 -0700

    Print size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,14 @@ int main(void)
 {	char c;
 	int i;
 	float f;
-	int li;
-	int lli;
+	unsigned long int li;
+	unsigned long long int lli;
 
-	printf("Size of Char: %lu byte(s) \n", (unsigned long)sizeof(c));
+	printf("Size of a Char: %lu byte(s) \n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s) \n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s) \n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s) \n", (unsigned long)sizeof(lli));
 	printf("Size of a float: %lu byte(s) \n", (unsigned long)sizeof(f));
-	printf("Size of a long long int: %lu byte(s) \n", (unsigned long long)sizeof(lli));
-
 	return (0);
 
 }
