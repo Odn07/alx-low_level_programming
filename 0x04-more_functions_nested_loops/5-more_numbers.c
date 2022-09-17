3fba32c9dba7e7e6637f1b2d4e50bed21e27ebaf
Emmanuel Odion <odione07@gmail.com>
2022-09-16 20:54:26 -0700

    Print 10 multiply by a number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+* more_numbers - Multiply give number by 10.
+*
+* Return: Return 0.
+*/
+
+void more_numbers(void)
+{
+	int i, j = 0;
+
+	for (; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
