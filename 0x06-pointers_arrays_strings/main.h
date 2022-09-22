1a97143dbbb9f5674292ca2cfd142bcd159f0185
Emmanuel Odion <odione07@gmail.com>
2022-09-22 15:28:35 -0700

    Reverse a string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <string.h>
 
+
+void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
