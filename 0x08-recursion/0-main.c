1d71ffe5e179b3e3bb32972c84d8f0d2104f3d47
Emmanuel Odion <odione07@gmail.com>
2022-09-28 19:22:12 -0700

    Prints string followed by a newline

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -8,6 +8,7 @@
 
 int main(void)
 {
-    _puts_recursion("Puts with recursion");
-    return (0);
+	_puts_recursion("First, solve the problem. Then, write the code.");
+	_puts_recursion("Talk is cheap. Show me the code.");
+	return (0);
 }
