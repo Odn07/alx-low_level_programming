ef373abd52ebeab6cf83594061a8652784913c13
Emmanuel Odion <odione07@gmail.com>
2022-10-05 22:08:14 -0700

    Concatenate all the argument of this program

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
 void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
